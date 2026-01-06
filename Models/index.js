3a6b75dfe9b988317fab112eae5c272f64065531
ordo-chao <princeben9312@gmail.com>
2026-01-06 14:14:14 +0300

    Feat: added paymnet info table and removed payment info association from users to each event

--- Models/index.js
+++ Models/index.js
@@ -2,6 +2,7 @@ import User from './User.model.js';
 import Event from './Event.model.js';
 import Ticket from './Ticket.model.js';
 import Attendee from './Attendee.model.js';
+import PaymentInfo from './paymentInfo.model.js';
 
 // Define relationships
 User.hasMany(Event, { foreignKey: 'organizer_id' });
@@ -19,5 +20,9 @@ Attendee.belongsTo(User, { foreignKey: 'user_id' });
 Ticket.hasMany(Attendee, { foreignKey: 'ticket_id' });
 Attendee.belongsTo(Ticket, { foreignKey: 'ticket_id' });
 
+Event.hasOne(PaymentInfo, { foreignKey: "event_id"});
+PaymentInfo.belongsTo(Event, { foreignKey: "event_id"});
 
-export { User, Event, Ticket, Attendee };
+
+
+export { User, Event, Ticket, Attendee , PaymentInfo};
