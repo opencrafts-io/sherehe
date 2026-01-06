9f00b06c283d5157b9c3b2f02ecc9af7138c0368
ordo-chao <princeben9312@gmail.com>
2026-01-06 15:24:40 +0300

    Fix: changed status code from 404 to 200 when getting event by organizer id is empty

--- Controllers/Event.controller.js
+++ Controllers/Event.controller.js
@@ -658,7 +658,7 @@ export const getEventbyOrganizerIdController = async (req, res) => {
     );
 
     if (!events || events.length === 0) {
-      return res.status(404).json([]);
+      return res.status(200).json([]);
     }
 
     return res.status(200).json({
