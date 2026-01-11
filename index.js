bfb94bf5f41e8dcef77dc6880d8fb0038f7dc934
ordo-chao <princeben9312@gmail.com>
2026-01-11 10:43:28 +0300

    TEST: test of whether env variables are working

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use('/purchase', purchaseTicket)
 app.listen(PORT, async () => {
   try {
     await sequelize.authenticate();
-    console.log("✅ Database connected...");
+    console.log("Database connected...");
 
     await sequelize.sync({ alter: true });
     console.log("✅ Models synced...");
