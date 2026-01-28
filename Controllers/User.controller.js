74084217d126c687c4c72ad55b45f96fe79800bb
ordo-chao <princeben9312@gmail.com>
2026-01-28 12:45:06 +0300

    Feat: added search users by username

--- Controllers/User.controller.js
+++ Controllers/User.controller.js
@@ -1,7 +1,8 @@
 import {
   createUserRepository,
   getUserByIdRepository,
-  updateUserRepository
+  updateUserRepository,
+  getUserByUsernameRepository
 } from '../Repositories/User.repository.js';
 
 import { logs } from '../Utils/logs.js';
@@ -111,6 +112,79 @@ export const updateUserController = async (req, res) => {
     );
 
     res.status(200).json(user);
+  } catch (error) {
+    const end = process.hrtime.bigint();
+    const durationMicroseconds = Number(end - start) / 1000;
+
+    logs(
+      durationMicroseconds,
+      "ERROR",
+      req.ip,
+      req.method,
+      error.message,
+      req.originalUrl,
+      500,
+      req.headers["user-agent"]
+    );
+
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+
+
+export const getUserByUsernameController = async (req, res) => {
+  const start = process.hrtime.bigint();
+
+  try {
+        const { q } = req.query;
+    const searchQuery = q?.trim() || "";
+
+       if (!searchQuery) {
+      const duration = Number(process.hrtime.bigint() - start);
+
+      logs(
+        duration,
+        "WARN",
+        req.ip,
+        req.method,
+        "Missing search query",
+        req.originalUrl,
+        400,
+        req.headers["user-agent"]
+      );
+
+      return res
+        .status(400)
+        .json({ error: "Missing search query parameter (?q=...)" });
+    }
+
+    const results = await getUserByUsernameRepository(searchQuery);
+
+    const end = process.hrtime.bigint();
+    const durationMicroseconds = Number(end - start) / 1000;
+
+    logs(
+      durationMicroseconds,
+      "INFO",
+      req.ip,
+      req.method,
+      "Search performed",
+      req.originalUrl,
+      200,
+      req.headers["user-agent"]
+    );
+    
+    if (!results || results.length === 0) {
+      return res.status(200).json([]);
+    }
+
+
+        return res.status(200).json({
+      count: results.length,
+      data: results,
+    });
   } catch (error) {
     const end = process.hrtime.bigint();
     const durationMicroseconds = Number(end - start) / 1000;
