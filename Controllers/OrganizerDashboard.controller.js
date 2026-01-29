8e5b9113bbbdeae65232801cb1377cc1eb60b2e5
ordo-chao <princeben9312@gmail.com>
2026-01-29 15:12:01 +0300

    Feat: added logs

--- Controllers/OrganizerDashboard.controller.js
+++ Controllers/OrganizerDashboard.controller.js
@@ -1,53 +1,128 @@
-import { getTicketByIdRepository, updateTicketRepository } from '../Repositories/Ticket.repository.js';
-import { getEventByIdRepository } from '../Repositories/Event.repository.js';
 import { logs } from '../Utils/logs.js';
-// import { getUserByIdRepository } from '../Repositories/User.repository.js';
-// import { sendPaymentRequest } from '../Middleware/Veribroke_sdk_push.js';
-// import { createTransactionRepository, getTransactionByIdRepository } from '../Repositories/Transactions.repository.js';
-// import { getPaymentInfoByEventIdRepository } from '../Repositories/paymentInfo.repository.js';
-import { createAttendeeRepository, getTotalAttendeesByEventIdRepository , getAttendeesByTicketIdRepository } from '../Repositories/Attendee.repository.js';
+import { getTotalAttendeesByEventIdRepository , getAttendeesByTicketIdRepository } from '../Repositories/Attendee.repository.js';
 import { getTotalScannersByEventIdRepository } from '../Repositories/eventScanners.repository.js';
 import {getEventTicketSalesStatsRepository} from '../Repositories/Ticket.repository.js';
-import { Op, Sequelize } from "sequelize";
 
 
 export const getEventStatsController = async (req, res) => {
+   const start = process.hrtime.bigint();
   try {
     const event_id = req.params.id;
     // Get total number of attendees
     const attendees = await getTotalAttendeesByEventIdRepository(event_id);
     const scanners = await getTotalScannersByEventIdRepository(event_id);
 
+        const end = process.hrtime.bigint();
+    const durationMicroseconds = Number(end - start) / 1000;
+
+    logs(
+      durationMicroseconds,
+      "INFO",
+      req.ip,
+      req.method,
+      "Event stats fetched successfully",
+      req.originalUrl,
+      201,
+      req.headers["user-agent"]
+    );
+
     res.status(200).json({
       attendees: attendees,
       scanners: scanners
     });
   } catch (error) {
-    console.error(error);
+        const end = process.hrtime.bigint();
+    const durationMicroseconds = Number(end - start) / 1000;
+
+    logs(
+      durationMicroseconds,
+      "ERROR",
+      req.ip,
+      req.method,
+      error.message,
+      req.originalUrl,
+      500,
+      req.headers["user-agent"]
+    );
     res.status(500).json({ error: 'Internal server error' });
   }
 }
 
 export const getTicketStatsController = async (req, res) => {
+    const start = process.hrtime.bigint();
   try {
     const event_id = req.params.id;
     const event = await getEventTicketSalesStatsRepository(event_id);
 
+        const end = process.hrtime.bigint();
+        const durationMicroseconds = Number(end - start) / 1000;
+    
+        logs(
+          durationMicroseconds,
+          "INFO",
+          req.ip,
+          req.method,
+          "Ticket stats fetched successfully",
+          req.originalUrl,
+          201,
+          req.headers["user-agent"]
+        );
+
    return res.status(200).json(event);
     
   } catch (error) {
-    console.error(error);
+        const end = process.hrtime.bigint();
+    const durationMicroseconds = Number(end - start) / 1000;
+
+    logs(
+      durationMicroseconds,
+      "ERROR",
+      req.ip,
+      req.method,
+      error.message,
+      req.originalUrl,
+      500,
+      req.headers["user-agent"]
+    );
     res.status(500).json({ error: 'Internal server error' });
   }
 }
 
 export const getAttendeesByTicketIdController = async (req, res) => {
+   const start = process.hrtime.bigint();
   try {
     const ticketId = req.params.id;
     const attendees = await getAttendeesByTicketIdRepository(ticketId);
+
+        const end = process.hrtime.bigint();
+    const durationMicroseconds = Number(end - start) / 1000;
+
+    logs(
+      durationMicroseconds,
+      "INFO",
+      req.ip,
+      req.method,
+      "Attendees fetched successfully",
+      req.originalUrl,
+      200,
+      req.headers["user-agent"]
+    );
+
     res.status(200).json(attendees);
   } catch (error) {
-    console.error(error);
+        const end = process.hrtime.bigint();
+    const durationMicroseconds = Number(end - start) / 1000;
+
+    logs(
+      durationMicroseconds,
+      "ERROR",
+      req.ip,
+      req.method,
+      error.message,
+      req.originalUrl,
+      500,
+      req.headers["user-agent"]
+    );
     res.status(500).json({ error: 'Internal server error' });
   }
 }
